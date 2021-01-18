3936c3507b805f474d198b241e1a52cb24f67998
Chuka Nwobi <chuka@circleci.com>
2021-01-18 17:40:20 +0100

    Add function type

--- src/hooks/useMouseMove.ts
+++ src/hooks/useMouseMove.ts
@@ -1,9 +1,10 @@
 import React, { useEffect } from 'react';
 
-export const useMouseMove = (
-  cursor: React.MutableRefObject<HTMLDivElement>
-) => {
-  const onMouseMove = (event: MouseEvent) => {
+type UseMouseMove = (cursor: React.MutableRefObject<HTMLDivElement>) => void;
+type OnMouseMove = (event: MouseEvent) => void;
+
+export const useMouseMove: UseMouseMove = cursor => {
+  const onMouseMove: OnMouseMove = event => {
     const { clientX, clientY } = event;
     cursor.current.style.left = `${clientX}px`;
     cursor.current.style.top = `${clientY}px`;
